ca5ad1577bfadc90e8fce87831e8765e043be4c1
Redglyph <redglyph@gmail.com>
2026-03-30 14:33:21 +0200

    minor update

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-632dd1fe.js',
+                path_to_root + 'searchindex-4bca4bc1.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
